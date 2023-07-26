0291621eb02f1aa176f61148dac304d9cc9fe148
Israel <israel@velocity.tech>
2023-07-26 12:53:21 +0200

    Initial Commit

---
+++ stocks.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import yfinance as yf
+
+# Define the ticker symbol
+tickerSymbol = 'AAPL'
+
+# Get data on the ticker
+tickerData = yf.Ticker(tickerSymbol)
+
+# Get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2010-1-1', end='2023-1-1')
+
+# Calculate the 50-day moving average
+tickerDf['MA50'] = tickerDf['Close'].rolling(window=50).mean()
+
+# Print the most recent data
+print(tickerDf.tail())
